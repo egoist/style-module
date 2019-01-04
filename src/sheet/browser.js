b2c2c0b0bfe37dea90be0de15796128a22571955
EGOIST <0x142857@gmail.com>
2019-01-05 02:13:14 +0800

    chore: clean up

--- src/sheet/browser.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-exports.__esModule = true
-exports['default'] = function() {
-  return typeof document === 'undefined'
-    ? null
-    : document.head.appendChild(document.createElement('style')).sheet
-}
